e8faa3a1edf97d6ae080583c8da221859a353173
jesusrpd <jesusrodrigo881@gmail.com>
2023-01-16 14:31:11 -0600

    nav mobil incompleted

--- components/general/Nav.jsx
+++ components/general/Nav.jsx
@@ -1,11 +1,20 @@
 export default function Nav(){
     return(
         <nav className="nav">
-            <div className="nav-hamburguer">
+            <input type="checkbox" id="check-menu" className="nav-hamburguer-check"/>
+            <div className="nav-hamburguer" forhtml="check-menu">
                 <div className="nav-hamburguer-line"></div>
                 <div className="nav-hamburguer-line"></div>
                 <div className="nav-hamburguer-line"></div>
             </div>
+            <div className="bg-opa"></div>
+            <ul className="nav-hamburguer-links">
+                <li className="nav-hamburguer-link">Inicio</li>
+                <li className="nav-hamburguer-link">Sobre Nosotros</li>
+                <li className="nav-hamburguer-link">¿Qué es?</li>
+                <li className="nav-hamburguer-link">Funcionalidades</li>
+                <li className="nav-hamburguer-link">Colabora</li>
+            </ul>
         </nav>
     )
 }
